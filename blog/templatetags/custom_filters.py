22e8cece3b1b99c3188e5bbe7380ff09472ff6e6
Ferhat Aslan <ferhatasln.4181@gmail.com>
2024-09-26 15:52:26 +0300

    first commit

---
+++ blog/templatetags/custom_filters.py
@@ -0,0 +1,10 @@
+import re
+from django import template
+from django.utils.html import strip_tags
+
+register = template.Library()
+
+@register.filter(name="strip_html")
+def strip_html(value):
+    
+    return strip_tags(value)
